719ce883b1b8cf7b9bc8875f31b9437bdbfdde4c
이아름 <areum6321@gmail.com>
2024-01-24 17:36:38 +0900

    상속관계 매핑 - 실행클래스 변경

--- src/main/java/jpabook/JpaMain.java
+++ src/main/java/jpabook/JpaMain.java
@@ -1,5 +1,6 @@
 package jpabook;
 
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -21,8 +22,11 @@ public class JpaMain {
 
         try {
 
-            Order order = new Order();
-            order.addOrderItem(new OrderItem());
+            Book book = new Book();
+            book.setName("book");
+            book.setAuthor("작가");
+
+            em.persist(book);
 
             tx.commit();
         } catch (Exception e) {
